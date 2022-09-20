43704f2ce8c628080d1ac414ce0266c9ac4abd61
Kashyap Patel <kapat@microsoft.com>
2022-09-20 16:40:46 -0400

    Add basic script to print raw payload length

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+var onnxRenderer = (function () {
+    "use strict";
+    return {
+        renderContent: function(rawContent, options) {
+
+            console.log("rawContent", rawContent);
+            console.log("options", options);
+
+            document.getElementById("rawContentLength").innerText = rawContent.length;
+
+            var renderUX = document.getElementById("model-display");
+            // TODO: Call render(rawPayload, renderUX) here
+        }
+    };
+}());
+
+VSS.init({
+    usePlatformScripts: true, 
+    usePlatformStyles: true, 
+    explicitNotifyLoaded: true 
+});
+
+VSS.ready(function () {
+    VSS.register("onnx_renderer", function (context) {
+        console.log("context", context);
+
+        return onnxRenderer;
+    });
+
+	VSS.notifyLoadSucceeded();
+});
